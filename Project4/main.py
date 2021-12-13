133820b036f63fcdeffa9bdc24ecbec30bde1fe6
Derek Logan <75442980+delo9146@users.noreply.github.com>
2021-12-13 16:07:45 -0700

    Update main.py

--- Project4/main.py
+++ Project4/main.py
@@ -1,4 +1,5 @@
 from racetrack import racetrack
+from valueIteration import valueIteration
 import numpy as np
 import string
 import random
@@ -9,14 +10,11 @@ tracks = race.racetrackBuilder('tracks')
 # starting velocity for all tracks & crash types
 startVelocity = [0,0]
 
-# For the L-track
+# For the L-track (example)
 l = tracks[0]
 # getting points figured out
 lStarts, lFinishes, lWalls, lPoints = race.startFinish(l)
 # picking random start point
 lStartingPoint = race.starting(lStarts)
 
-print(lStartingPoint)
-
-
-
+valueIteration.valMain(l, lPoints, lFinishes, lWalls, lStartingPoint)
