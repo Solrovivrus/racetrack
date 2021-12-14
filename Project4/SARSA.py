719fc23c913ebd74484b1e25705361c8ed15626f
Solrovivrus <77756366+Solrovivrus@users.noreply.github.com>
2021-12-13 18:23:59 -0700

    Update SARSA.py

--- Project4/SARSA.py
+++ Project4/SARSA.py
@@ -2,8 +2,7 @@ import os
 import numpy as np
 import pandas as pd
 import random
-import racetrack
-import racetrack
+from racetrack import racetrack
 
 '''Contributors: Alyse Mize 
 ---------------------------------------------------------------------------------------------- 
@@ -20,11 +19,11 @@ using the policy derived from Q and then update Q given the past Q.
 
 class SARSA:
     # --------------------------------------------------------------------------------------------
-    # chooseAction() will pick an action for the race car
-
+    # chooseAction() will pick an action for the race car.  If the car is above the learning rate of epsilon it will
+    # then choose actions that are slightly less random as it prioritize more "good" actions over random actions.
     def chooseAction(raceTrack, states, epsilon):
 
-        values = raceTrack[states, :]
+        values = [raceTrack [states, :]]
         maxStates = max(values)
         actions = [action for action in range(len(values))]
         greed = [action for action in range(len(values)) if values[action] == maxStates]
@@ -35,37 +34,73 @@ class SARSA:
         else:
             return random.choice(greed)
 
+    # --------------------------------------------------------------------------------------------
+    # updateAction() will update the reward system for the track to better decide for the greedy algorithm which
+    # algorithms are better/worse and which ones to choose.
     def updateAction(raceTrack, currentState, currentAction, reward, nextAction, nextState, alpha, gamma):
         racetrackCurrent = racetrack[currentState, currentAction]
         raceTrackNext = raceTrack[nextState, nextAction]
         updateReward = (reward + gamma * raceTrackNext) - racetrackCurrent
         racetrackCurrent = racetrackCurrent + alpha * updateReward
 
+    # --------------------------------------------------------------------------------------------
+    # get_racetrack() returns an empty array which will set up our reward system for which spots are better/worse.
+    # for the finish/starting line those spots are 1and .5 and for walls/track the values are zero.  The idea being that
+    # if the car hits a wall the updateTrack function will make that reward spot a -1 to teach the car to not hit that
+    # area.  The rest of the track however will be updated with values depending on how good/bad they are.
+    def get_racetrack(racetracks):
+        racetrack = []
+        row = []
+        for arr in racetracks:
+            racetrack.append(row)
+            string = arr[0]
+            string = string.replace("#", "0")
+            string = string.replace(".", "0")
+            string = string.replace("S", ".5")
+            string = string.replace("F", "1")
+            for c in string:
+                row.append(int(c))
+            row = []
+        return racetrack
+
+
+    # --------------------------------------------------------------------------------------------
+    # The trainCar() function is the main function for SARSA as it connects to all other related functions.  This
+    # function sets up all the variables such as epsilon, alpha, and gamma which are the rates of learning for the
+    # training function.  It will then go through a number of iterations to train the car into reaching the finish line
+    # because the finish line has the highest value of 1 it incentives the car to reach that line as it has the highest
+    # value. By iterating and picking random actions that lead to random spots the car will travel around the board
+    # until it finds the best route, hypothetically.
     def trainCar(racetracks):
         Sar = SARSA
+        race = racetrack
+        lStarts, lFinishes, lWalls, lPoints = race.startFinish(racetracks)
         maxRaces = 1000
         maxState = 100
         epsilon = .9
         alpha = .85
         gamma = .95
-        victory = 0  # and by "victory" it could mean either crossing the finish line of just not crashing
         crashes = 0
         reward = 0
-        racetrack = racetracks
+        velocity = [0, 0]
+        track = Sar.get_racetrack(racetracks)
+        possAccel = [[0,0], [0,-1], [0,1], [-1,0], [-1,-1], [-1,1], [1,0], [1,-1], [1,1]]
+        possAccel = random.choice(possAccel)
 
-        for race in range(maxRaces):
-            initialState = racetrack.reset()
-            initialAction = Sar.chooseAction(racetrack, initialState, epsilon)
-            victory = 0
+        for i in range(maxRaces):
+            initialState = track
+            initialAction = Sar.chooseAction(track, initialState, epsilon)
 
             for state in range(maxState):
-                nextState, reward, done, info = racetrack.step(initialAction)
+                nextState, velocity, crossedFinish, reward = race.updateTrack(initialState, velocity, possAccel, lFinishes, lWalls, racetracks)
 
-                nextAction = Sar.chooseAction(racetrack, nextState, epsilon)
+                nextAction = Sar.chooseAction(track, nextState, epsilon)
 
-                Sar.updateAction(racetrack, initialState, initialAction, reward, nextAction, nextState, alpha, gamma)
+                Sar.updateAction(track, initialState, initialAction, reward, nextAction, nextState, alpha, gamma)
 
                 initialState, initialAction = nextState, nextAction
 
-                if done:
+                if crossedFinish:
+                    print("You crossed the finish line!")
+                    print(track)
                     break
